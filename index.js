18d51776dab7e5029738f10d7c4a31e9b0942261
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-11-04 14:09:34 -0400

    mongo connected

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('mongo connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(process.env.PORT || 3333, () => {
+  console.log('backend server running');
+});
