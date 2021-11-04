735e472020834aba4512de6a349064b546305646
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-11-04 18:52:42 -0400

    mongo connected

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
+
+app.use(express.json());
 
 dotenv.config();
 
@@ -15,10 +18,10 @@ mongoose
     console.log(err);
   });
 
-app.use('/api/user', userRoute);
+// Routes
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('backend server running');
 });
-
-console.log(process.env.PORT);
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
 
-router.get('/usertest', (req, res) => {
-  res.send('The user test is working');
-});
+
 
 module.exports = router;
