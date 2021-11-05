ebcc5cc89f054be260f32d32895aec49aaf59c82
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-11-04 22:49:08 -0400

    sign up complete

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,38 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const CryptoJS = require('crypto-js');
 
 // Register
 router.post('/register', async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
   });
   try {
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
   } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Login
+
+router.post('/login', async (req, res) => {
+  try {
+    const user = await user.findOne({ username: req.body.username });
+    !user && res.status(401).json("user not found")
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const password = hashedPassword.toString(CryptoJS.enc.Utf8);
+    !password !== req.body.password && res.status(401).json("wrong password")
+  } catch {
     res.status(500).json(err);
   }
 });
