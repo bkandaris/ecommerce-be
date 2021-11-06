2e6c5ffcba2e6127fdf584460a615f8364ce7b35
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-11-05 21:31:05 -0400

    cart route completed

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
 
 app.use(express.json());
 
@@ -23,6 +24,7 @@ mongoose
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/products', productRoute);
+app.use('/api/cart', cartRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('backend server running');
