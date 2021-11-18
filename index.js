496d55f22e07ef4e073c138f94b1fdb9bf3e7bc4
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-11-17 19:08:21 -0500

    stripe fixed a bit

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
 const orderRoute = require('./routes/order');
+const stripeRoute = require('./routes/stripe');
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 
 dotenv.config();
@@ -27,6 +30,7 @@ app.use('/api/auth', authRoute);
 app.use('/api/products', productRoute);
 app.use('/api/carts', cartRoute);
 app.use('/api/orders', orderRoute);
+app.use('/api/checkout', stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('backend server running');
--- routes/stripe.js
+++ routes/stripe.js
@@ -1,19 +1,20 @@
 const router = require('express').Router();
+// const stripe = require("stripe")(process.env.STRIPE_KEY);
+const KEY = process.env.STRIPE_KEY;
+const stripe = require('stripe')(KEY);
 
-const stripe = require('stripe')(process.env.STRIPE_KEY);
-
-router.post('payment', (req, res) => {
+router.post('/payment', (req, res) => {
   stripe.charges.create(
     {
       source: req.body.tokenId,
       amount: req.body.amount,
       currency: 'usd',
     },
-    (stripeError, stripeResponse) => {
-      if (stripeError) {
-        res.status(500).json(stripeError);
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        res.status(500).json(stripeErr);
       } else {
-        res.status(200).json(stripeResponse);
+        res.status(200).json(stripeRes);
       }
     }
   );
